c2df8148419c766410a21cfe968e34cd2e261700
mhmd.mbt <mo.noori2014@gmail.com>
2025-02-27 13:31:21 -0500

    Update pom.xml

--- src/test/java/com/devsecops/NumericApplicationTests.java
+++ src/test/java/com/devsecops/NumericApplicationTests.java
@@ -1,11 +1,11 @@
 package com.devsecops;
 
-import org.junit.jupiter.api.Test; // تغییر به JUnit 5
-import org.junit.jupiter.api.extension.ExtendWith; // تغییر به JUnit 5
+import org.junit.jupiter.api.Test; 
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension; // تغییر به JUnit 5
+import org.springframework.test.context.junit.jupiter.SpringExtension; 
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -13,7 +13,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@ExtendWith(SpringExtension.class) // تغییر به JUnit 5
+@ExtendWith(SpringExtension.class) 
 @SpringBootTest
 @AutoConfigureMockMvc
 public class NumericApplicationTests {
