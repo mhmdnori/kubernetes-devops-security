21694ff958a101253cb50164f815dcea03678f0a
mhmd.mbt <mo.noori2014@gmail.com>
2025-03-24 05:09:33 -0400

    Update zap.sh

--- zap.sh
+++ zap.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
-PORT=$(kubectl -n default get svc devsecops-svc -o json | jq .spec.ports[].nodePort)
+PORT=$(kubectl -n default get svc devsecops-svc -o json | jq -r '.spec.ports[0].nodePort')
 
-chmod 777 $(pwd)
-docker run --rm -v ${workspace}:/zap -t zaproxy/zap-stable zap/zap-api-scan.py -t $APPLICATION_URL:$PORT/v3/api-docs -f openapi -r zap-report.html
+chmod 755 $(pwd)
+
+docker run --rm -v $(pwd):/zap -t zaproxy/zap-stable /zap/zap-api-scan.py \
+    -t "$APPLICATION_URL:$PORT/v3/api-docs" \
+    -f openapi \
+    -r /zap/owasp-zap-report/zap-report.html
 
 exit_code=$?
 
-sudo mkdir -p owasp-zap-report
-sudo mv zap-report.html owasp-zap-report
+mkdir -p owasp-zap-report
 
-if [[${exit_code} -ne 0]]; then
-    exit 1;
-fi;
+if [ "$exit_code" -ne 0 ]; then
+    exit 1
+fi
