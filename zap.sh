d75e8e4eb7d491f533e2ffea9dd3045a3bf96da4
mhmd.mbt <mo.noori2014@gmail.com>
2025-03-24 06:11:34 -0400

    Update bash.sh

--- zap.sh
+++ zap.sh
@@ -4,7 +4,7 @@ PORT=$(kubectl -n default get svc devsecops-svc -o json | jq -r '.spec.ports[0].
 
 chmod 755 $(pwd)
 
-docker run --rm -v $(pwd):/zap -t zaproxy/zap-stable /zap/zap-api-scan.py \
+docker run --rm --network host -v $(pwd):/zap/wrk/:rw -t zaproxy/zap-stable zap-api-scan.py \
     -t "$APPLICATION_URL:$PORT/v3/api-docs" \
     -f openapi \
     -r /zap/owasp-zap-report/zap-report.html
@@ -12,6 +12,7 @@ docker run --rm -v $(pwd):/zap -t zaproxy/zap-stable /zap/zap-api-scan.py \
 exit_code=$?
 
 mkdir -p owasp-zap-report
+mv zap-report.html owasp-zap-report
 
 if [ "$exit_code" -ne 0 ]; then
     exit 1
